3ac28c3cc07f498e85b5784b96300617d722a60d
Shriti Kumari <shritisingha7@gmail.com>
2026-06-26 13:06:25 +0530

    Delete Lec2(practice questions)p3.py

--- Lec2(practice questions)p3.py
+++
@@ -1,9 +0,0 @@
-#Let‘s Practice
-#WAP to input user’s first name & print its length.
-
-name = input("enter your name :")
-print("length of your name is", len(name))
-
-#WAP to find the occurrence of ‘$’ in a String
-str = "hi $ i am the$ symbol $200" 
-print(str.count("$"))
