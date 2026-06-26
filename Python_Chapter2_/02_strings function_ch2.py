29abc4149b9ece4cae34949f5a98d7961dc975eb
Shriti Kumari <shritisingha7@gmail.com>
2026-06-26 13:01:29 +0530

    Add files via upload

---
+++ Python_Chapter2_/02_strings function_ch2.py
@@ -0,0 +1,30 @@
+#String function
+#endswith
+str = "i complete my art degree form shilphi niketan."
+print(str.endswith("shilphi niketan."))
+print(str.endswith("tan."))
+print(str.endswith("ten."))
+
+#capitalize
+print(str.capitalize())
+
+#replace
+print(str.replace("e","a"))
+
+#find
+print(str.find("my"))
+print(str.find("degree"))
+print(str.find("complete"))
+
+#count
+print(str.count("e"))
+print(str.count("te"))
+print(str.count("hi"))
+
+
+
+
+
+
+
+
