ef94191b62fabdef181ddc8a18d20f748e67ec46
Shriti Kumari <shritisingha7@gmail.com>
2026-06-26 12:49:17 +0530

    Add files via upload

---
+++ Python_Chapter1_/05_Practiceproblem_ch1.py
@@ -0,0 +1,30 @@
+#practiceproblem
+a = int(input("first number ="))
+b = int(input("second number ="))
+
+sum = a + b
+
+print(sum)
+
+#Write a Program to input 2 numbers & print their sum.
+first = int(input("enter first = "))
+second = int(input("enter second ="))
+
+print("sum= ", first + second)
+
+#WAP to input side of a square & print its area
+side =  int(input("Side of a sqaure ="))
+
+print("area=",(side*side))
+
+#WAP to input 2 floating point numbers & print their average
+a = float(input("first number = "))
+b = float(input("second number = "))
+
+print("average = ", (a + b)/2)
+
+#WAP to input 2 int numbers, a and b. 
+#Print True if a is greater than or equal to b. If not print False
+
+p = int(input("first number="))
+q = int(input("second number="))
