d3fec8691b6a07419e770a6b5386423c22cbd0c0
Shriti Kumari <shritisingha7@gmail.com>
2026-06-26 10:09:53 +0530

    Update Lec1(Sumprogram)p4.py

--- Lec1(Sumprogram)p4.py
+++ Lec1(Sumprogram)p4.py
@@ -1,6 +1,23 @@
-a = 10000
-b =7888
+#multiply
+a = 567
+b =78
 multiply = a * b
-
 print(multiply)
+
+#sum
+a = 566
+b =754
+sum = a + b
+print(sum)
+
+#substract
+a = 1789
+b =543
+substract = a - b
+print(substract)
+
+#divide
+a = 45
+b =5
+divide = a / b
 
